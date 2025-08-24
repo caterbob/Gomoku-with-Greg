43c7276a032727ab54095c76c8c43df302191386
caterbob <trickshotkoopa2007@gmail.com>
2025-08-24 10:44:55 -0500

    Move List converted to LocationList

--- GomokuAIproject/Greg3/G3VirtualBoard.java
+++ GomokuAIproject/Greg3/G3VirtualBoard.java
@@ -1,5 +1,6 @@
 package GomokuAIproject.Greg3;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -110,23 +111,23 @@ public class G3VirtualBoard extends VirtualBoard{
         getEvaluation();
     }
 
-    public ArrayList<Integer> getCandidateMoves(){
-        ArrayList<Integer> moves = new ArrayList<Integer>();
+    public LocationList getCandidateMoves(){
+        LocationList moves = new LocationList();
 
         // finds moves that are adjacent to stones already placed
         for(int i = 0; i < 169; i++){
             if(super.getCellValue(i) != BoardConstants.EMPTY){
                 for(int offset: OffsetConstants.REAL_OFFSETS){
-                    if(isMoveValid(i, offset) && !moves.contains(i + offset))
-                        moves.add(i + offset);
+                    if(isMoveValid(i, offset) && !moves.containsLocation(i + offset))
+                        moves.addLocation(i + offset);
                     // if(isMoveValid(i + offset, offset) && !moves.contains(i + 2 * offset))
                     //     moves.add(i + 2 * offset);
                 }
             }
         }
 
-        if(moves.size() == 0){
-            moves.add(84);  // if stones placed yet, add possible move at center
+        if(moves.getSize() == 0){
+            moves.addLocation(84);  // if stones placed yet, add possible move at center
         }
         return moves;
     }
--- GomokuAIproject/Greg3/LocationList.java
+++ GomokuAIproject/Greg3/LocationList.java
@@ -6,37 +6,48 @@ public class LocationList {
     private int[] locationInstances;
     private int[] locations;
     private int size;
+    private int start; // where to start in locations (needed because of buffer space)
     public int[] scratch;   // used as temp int[] for locations
+
+    private static final int NON_EXISTENT = -2;
+    private static final int ENTIRE_ARRAY_SIZE = 250;
+    private static final int BUFFER_SIZE = ENTIRE_ARRAY_SIZE - 169;
 
     public LocationList(){
         locationInstances = new int[169];
-        locations = new int[169];
-        scratch = new int[169];
+        locations = new int[250];   // bigger than 169 to offer buffer space
+        scratch = new int[250];
         Arrays.fill(locationInstances, 0);
         Arrays.fill(locations, -1);
         Arrays.fill(scratch, -1);
+        start = BUFFER_SIZE;
         size = 0;
     }
 
     public void addLocation(int location){
         if(locationInstances[location] == 0){
-            locations[size++] = location;
+            locations[start + size++] = location;
         }
         locationInstances[location]++;
         //System.out.println(locationInstances[location]);
     }
 
     public int getLocation(int index){
-        return locations[index];
+        return locations[start + index];
     }
 
     public int getSize(){
         return size;
     }
 
+    public int getStart(){
+        return start;
+    }
+
     public void clear(){
         Arrays.fill(locationInstances, 0);
         Arrays.fill(locations, -1);
+        start = BUFFER_SIZE;
         size = 0;
     }
 
@@ -47,36 +58,60 @@ public class LocationList {
     // combines this location list with another
     public void combine(LocationList other){
         for(int i = 0; i < other.getSize(); i++){
-            addLocation(other.getLocation(i));
+            addLocation(other.getLocation(i));  // getLocation handles start
         }
     }
 
     // locations must be compacted later!
     public void subtract(LocationList other){
         for(int i = 0; i < other.getSize(); i++){
-            locationInstances[other.getLocation(i)]--;
+            locationInstances[other.getLocation(i)]--;  //getLocation handles start
         }
-        compactLocations();
+        removeGoneLocations();
     }
 
-    // remove locations that no longer exist. Helper fucntion in subtract()
-    private void compactLocations(){
-        int scratchIndex = 0;
+    // remove locations that no longer exist according to locationInstances
+    //Helper function in subtract()
+    private void removeGoneLocations(){
+        int scratchIndex = BUFFER_SIZE;
         Arrays.fill(scratch, -1);
-        for(int i = 0; i < size; i++){
+        for(int i = start; i < start + size; i++){
             if(locationInstances[locations[i]] > 0)
                 scratch[scratchIndex++] = locations[i];
         }
         System.arraycopy(scratch, 0, locations, 0, scratch.length);
-        size = scratchIndex;
+        size = scratchIndex - BUFFER_SIZE;
+        start = BUFFER_SIZE;
+    }
+
+    // removes locations that have been moved in the array
+    // Helper function for bringToFront
+    private void compactLocations(){
+        int scratchIndex = BUFFER_SIZE;
+        Arrays.fill(scratch, -1);
+        for(int i = 0; i < size + 1; i++){  // size + 1 to account for new NON_EXISTENT location
+            if(getLocation(i) == NON_EXISTENT)
+                continue;
+            scratch[scratchIndex++] = getLocation(i);
+        }
+        System.arraycopy(scratch, 0, locations, 0, scratch.length);
+        size = scratchIndex - BUFFER_SIZE;
+        start = BUFFER_SIZE;
+    }
+
+    public void bringLocationToFront(int index){
+        int locationConsidered = getLocation(index);
+        locations[start + index] = NON_EXISTENT;
+        start--;
+        locations[start] = locationConsidered;
+        compactLocations();
     }
 
     public boolean hasOverlap(LocationList other){
-        for(int i = 0; i < size; i++){
+        for(int i = start; i < start + size; i++){
             if(other.containsLocation(locations[i]))
                 return true;
         }
         return false;
     }
-
 }
